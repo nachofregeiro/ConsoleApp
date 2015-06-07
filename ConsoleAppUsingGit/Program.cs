d05e0b396a8d918c4e917515935098f8c6bb1844
Nacho Fregeiro <nachofregeiro@gmail.com>
2015-06-07 19:58:27 -0300

    More changes in the branch

--- ConsoleAppUsingGit/Program.cs
+++ ConsoleAppUsingGit/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleAppUsingGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello world!");
+            Console.WriteLine("Another line...");
         }
     }
 }
